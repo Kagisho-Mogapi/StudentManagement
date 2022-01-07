277e0eeed994228a4b44a4f6f876a52d6cf8f400
Kagisho Mogapi <mogapikagisho@gmail.com>
2022-01-07 12:36:41 +0200

    Add project files.

---
+++ Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagement
+{
+    internal class Person
+    {
+        string fName;
+        string lName;
+        string studID;
+        string gender;
+        byte age;
+        string level;
+
+        public Person(string fName, string lName, string studID, string gender, byte age, string level)
+        {
+            this.fName = fName;
+            this.lName = lName;
+            this.studID = studID;
+            this.gender = gender;
+            this.age = age;
+            this.level = level;
+        }
+    }
+}
